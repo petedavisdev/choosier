252411b6ccd2bc65b932fb5c5b4ba59c7338e9fa
petedavisdev <oldmanplanet@gmail.com>
2022-07-07 15:36:23 +0100

    Tweak voting logic

--- app.vue
+++ app.vue
@@ -49,6 +49,55 @@ function advanceChosen(option, index) {
 }
 </script>
 
+<template>
+  <form v-for="(match, matchIndex) in state.matches" class="match">
+    <!-- Matches -->
+    <section class="options">
+      <label v-for="option in match">
+        <input
+          type="radio"
+          :name="'option' + matchIndex"
+          @input="advanceChosen(matchIndex, option)"
+          required
+        />
+        {{ option }}
+      </label>
+    </section>
+
+    <aside class="controls">
+      <button type="reset" @click="advanceChosen(matchIndex)">Back</button>
+    </aside>
+
+    <template v-else-if="matchIndex === length - 1 && match[0]">
+      <input disabled :value="match[0]" />
+      {{ match[0] }}
+    </template>
+  </form>
+</template>
+
+<script setup lang="ts">
+const options = ['A', 'B', 'C', 'D', 'E'];
+const length = options.length;
+
+const state = reactive({
+  matches: [],
+});
+
+state.matches = options.map((_option, index) => [
+  options[2 * index],
+  options[2 * index + 1],
+]);
+
+function advanceChosen(index: number, option?: string) {
+  const match = Math.floor((length + index) / 2);
+  const position = (length + index) % 2;
+
+  console.log({ match, position });
+
+  state.matches[match][position] = option;
+}
+</script>
+
 <style>
 * {
   box-sizing: border-box;
