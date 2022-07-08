f05cdd7300baabcfff611ef47db7c9a2662bd6e0
Pete Davis <7900180+petedavisdev@users.noreply.github.com>
2022-07-08 14:31:48 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,57 +1,6 @@
 <template>
   <form v-for="(match, matchIndex) in state.matches" class="match">
     <!-- Matches -->
-    <template v-if="matchIndex < length - 1 && match[1]">
-      <section class="options">
-        <label v-for="option in match">
-          <input
-            type="radio"
-            :name="matchIndex"
-            @input="advanceChosen(option, matchIndex)"
-            required
-          />
-          {{ option }}
-        </label>
-      </section>
-
-      <aside class="controls">
-        <button type="reset" @click="advanceChosen(undefined, index)">
-          Back
-        </button>
-      </aside>
-    </template>
-
-    <template v-else-if="matchIndex === length - 1 && match[0]">
-      <input disabled :value="match[0]" />
-      {{ match[0] }}
-    </template>
-  </form>
-</template>
-
-<script setup lang="ts">
-const options = ['A', 'B', 'C', 'D', 'E'];
-const length = options.length;
-
-const state = reactive({
-  matches: [],
-});
-
-state.matches = options.map((option, index) => [
-  options[2 * index],
-  options[2 * index + 1],
-]);
-
-function advanceChosen(option, index) {
-  const match = Math.floor((length + index) / 2);
-  const position = (length + index) % 2;
-
-  state.matches[match][position] = option;
-}
-</script>
-
-<template>
-  <form v-for="(match, matchIndex) in state.matches" class="match">
-    <!-- Matches -->
     <section class="options">
       <label v-for="option in match">
         <input
