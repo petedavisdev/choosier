a9b27577c9920fe301edaf11bc990afe41b06e5c
petedavisdev <oldmanplanet@gmail.com>
2022-10-19 17:59:10 +0200

    FIx ios styles

--- pages/new.vue
+++ pages/new.vue
@@ -1,24 +1,22 @@
 <template>
 	<main>
-		<section>
+		<form class="box" action="">
 			<Logo class="Logo" />
 			<h1>You're early!</h1>
 			<p>While we're beta testing, adding new choices is invite-only.</p>
 
-			<form action="">
-				<p class="Field">
-					<label for="email">Email *</label>
-					<input type="email" id="email" name="email" required />
-				</p>
-				<p class="Field">
-					<label for="instagram">Instagram username</label>
-					<input id="instagram" name="instagram" required />
-				</p>
-				<footer>
-					<button class="button">Join the waiting list</button>
-				</footer>
-			</form>
-		</section>
+			<p class="Field">
+				<label for="email">Email *</label>
+				<input type="email" id="email" name="email" required />
+			</p>
+			<p class="Field">
+				<label for="instagram">Instagram username</label>
+				<input id="instagram" name="instagram" required />
+			</p>
+			<footer>
+				<button class="button">Join the waiting list</button>
+			</footer>
+		</form>
 	</main>
 </template>
 
@@ -40,7 +38,7 @@ main {
 	display: grid;
 	grid-template-columns: auto 1fr;
 	gap: 0.5em;
-	align-items: baseline;
+	align-items: center;
 }
 
 footer {
