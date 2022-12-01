a5b00be09e7d3eafbf9b8d59e22be2ce687b6c8d
petedavisdev <oldmanplanet@gmail.com>
2022-12-01 21:58:03 +0000

    New choice layout

--- components/NewChoice.vue
+++ components/NewChoice.vue
@@ -1,62 +1,74 @@
 <template>
 	<form>
-		<h2><label for="title">Title</label></h2>
-		<input v-model="data.title" class="TitleInput" maxlength="25" required />
-		<small>E.g. "Which color is best?". Up to 25 characters</small>
+		<section>
+			<h2><label for="title">Title</label></h2>
+			<input v-model="data.title" class="TitleInput" maxlength="25" required />
+			<small>E.g. "Which color is best?". Up to 25 characters</small>
+		</section>
 
-		<h2>Images</h2>
-		<p>You can add 2 to 10 images URLs</p>
-
-		<input
-			v-for="(url, index) in data.imageValues"
-			:key="index"
-			v-model="data.imageValues[index]"
-			placeholder="https://..."
-			class="ImageInput"
-			:required="index < 2"
-		/>
-
-		<h2>Category</h2>
-		<p v-for="(category, index) in categories" :key="index">
-			<label>
-				<input
-					type="radio"
-					v-model="data.category"
-					:value="category"
-					required
+		<section>
+			<h2>Listing preview</h2>
+			<div class="grid">
+				<NewChoicePreview
+					:images="imageURLs"
+					:title="data.title"
+					:username="profile.username"
 				/>
-				{{ category }}
-			</label>
-		</p>
+			</div>
+		</section>
 
-		<h2>Visibility</h2>
-		<p>
-			You have {{ profile.credits }} credit{{
-				profile.credits === 1 ? '' : 's'
-			}}. <NuxtLink to="/credits">Get more credits</NuxtLink>
-		</p>
-		<p v-for="(text, name) in visibility" :key="name">
-			<label>
-				<input
-					type="radio"
-					v-model="data.visibility"
-					:value="name"
-					:disabled="name === 'Private'"
-					required
-				/>
-				{{ name }} <small>{{ text }}</small>
-			</label>
-		</p>
-
-		<h2>Listing preview</h2>
-		<section class="grid">
-			<NewChoicePreview
-				:images="imageURLs"
-				:title="data.title"
-				:username="profile.username"
+		<section class="fullwidth">
+			<h2>Images</h2>
+			<p>You can add 2 to 10 images URLs</p>
+			<input
+				v-for="(url, index) in data.imageValues"
+				:key="index"
+				v-model="data.imageValues[index]"
+				placeholder="https://..."
+				class="ImageInput"
+				:required="index < 2"
 			/>
 		</section>
-		<button type="submit" class="button">Save and publish</button>
+
+		<section>
+			<h2>Category</h2>
+			<p v-for="(category, index) in categories" :key="index">
+				<label>
+					<input
+						type="radio"
+						v-model="data.category"
+						:value="category"
+						required
+					/>
+					{{ category }}
+				</label>
+			</p>
+		</section>
+
+		<section>
+			<h2>Visibility</h2>
+			<p>
+				You have {{ profile.credits }} credit{{
+					profile.credits === 1 ? '' : 's'
+				}}. <NuxtLink to="/credits">Get more credits</NuxtLink>
+			</p>
+			<p v-for="(text, name) in visibility" :key="name">
+				<label>
+					<input
+						type="radio"
+						v-model="data.visibility"
+						:value="name"
+						:disabled="name === 'Private'"
+						required
+					/>
+					{{ name }} <small>{{ text }}</small>
+				</label>
+			</p>
+		</section>
+
+		<section>
+			<button type="submit" class="button">Save and publish</button>
+		</section>
 	</form>
 </template>
 
@@ -91,10 +103,15 @@ const imageURLs = computed(() => data.imageValues.filter((value) => value));
 </script>
 
 <style scoped>
+form {
+	display: grid;
+	gap: 1em;
+}
+
 .TitleInput {
 	display: block;
-	width: 25em;
-	max-width: 100%;
+	width: 100%;
+	max-width: 25em;
 }
 
 .ImageInput {
@@ -109,5 +126,15 @@ const imageURLs = computed(() => data.imageValues.filter((value) => value));
 
 .ImageInput:placeholder-shown + .ImageInput:nth-of-type(n + 8) {
 	display: none;
+}
+
+@media (min-width: 800px) {
+	form {
+		grid-template-columns: 1fr 1fr;
+	}
+
+	.fullwidth {
+		grid-column: span 2;
+	}
 }
 </style>
