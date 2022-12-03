2b1bf4338f8ef20c4948ba13035dd793f97d989f
petedavisdev <oldmanplanet@gmail.com>
2022-12-03 17:27:33 +0000

    Cover card style

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-	<NuxtLink :to="`/${id}`" class="Card">
+	<NuxtLink :to="`/${id}`" class="Card button">
 		<div class="Images">
 			<span
 				class="Image"
@@ -29,10 +29,6 @@ const choice = await useChoice(props.id);
 	grid-template-areas: 'x';
 	padding: 0;
 	text-decoration: none;
-	border-radius: 1em;
-	overflow: hidden;
-	--box-shadow: -1px 2px 5px 2px #0003;
-	box-shadow: var(--box-shadow);
 }
 
 .Images {
@@ -51,9 +47,7 @@ const choice = await useChoice(props.id);
 	justify-self: start;
 	align-self: end;
 	background-color: var(--light);
-	margin: 1em;
 	padding: 0.5em 1em;
-	border-radius: 0.5em;
 	box-shadow: var(--box-shadow);
 	transition: transform 0.3s;
 }
@@ -61,11 +55,9 @@ const choice = await useChoice(props.id);
 .TitleIntro {
 	display: block;
 	font-weight: normal;
-	color: #333;
 }
 
 .Card:hover .Title {
 	background-color: white;
-	transform: translate(1px, -1px);
 }
 </style>
