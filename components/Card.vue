e5dae91c24f3206313dccf543ef0d0db843329ed
petedavisdev <oldmanplanet@gmail.com>
2022-12-03 16:56:54 +0000

    Overlay card

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-	<NuxtLink :to="`/${id}`" class="Card button">
+	<NuxtLink :to="`/${id}`" class="Card">
 		<div class="Images">
 			<span
 				class="Image"
@@ -8,10 +8,10 @@
 			></span>
 		</div>
 
-		<h3 class="Title">
+		<div class="Title">
 			<small class="TitleIntro">Help @{{ choice.username }} choose</small>
-			{{ choice.title }}
-		</h3>
+			<strong>{{ choice.title }}</strong>
+		</div>
 	</NuxtLink>
 </template>
 
@@ -24,29 +24,48 @@ const choice = await useChoice(props.id);
 
 <style scoped>
 .Card {
-	padding: 0.75em 0.75em 0;
-	aspect-ratio: 1.91/1;
-	display: flex;
-	flex-direction: column;
+	aspect-ratio: 16 / 9;
+	display: grid;
+	grid-template-areas: 'x';
+	padding: 0;
+	text-decoration: none;
+	border-radius: 1em;
+	overflow: hidden;
+	--box-shadow: -1px 2px 5px 2px #0003;
+	box-shadow: var(--box-shadow);
 }
 
 .Images {
-	flex-grow: 1;
-	display: flex;
-	align-items: stretch;
-	gap: 0.125em;
-	overflow: hidden;
+	grid-area: x;
+	display: grid;
+	grid-auto-flow: column;
 }
 
 .Image {
-	flex-grow: 1;
 	background-size: cover;
 	background-position: center;
+}
+
+.Title {
+	grid-area: x;
+	justify-self: start;
+	align-self: end;
+	background-color: var(--light);
+	margin: 1em;
+	padding: 0.5em 1em;
+	border-radius: 0.5em;
+	box-shadow: var(--box-shadow);
+	transition: transform 0.3s;
 }
 
 .TitleIntro {
 	display: block;
 	font-weight: normal;
 	color: #333;
+}
+
+.Card:hover .Title {
+	background-color: white;
+	transform: translate(1px, -1px);
 }
 </style>
