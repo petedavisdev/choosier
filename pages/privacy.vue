4ca3999bc66892df4983aa0cba2d4ea3491d477e
petedavisdev <oldmanplanet@gmail.com>
2022-12-04 15:10:49 +0000

    Privacy and terms

---
+++ pages/privacy.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>Privacy policy</h1>
+	<p>
+		Choosier only uses essential cookies. No analytics cookies. No third-party
+		cookies.
+	</p>
+	<p>
+		Choosier only collects email addresses for the purposed of managing account
+		access. We use Supabase and Google for login services.
+	</p>
+	<p>Your email address will not be shared with third-parties.</p>
+</template>
---
+++ pages/terms.vue
@@ -0,0 +1,24 @@
+<template>
+	<h1>Terms of service</h1>
+
+	<h2>Content rules</h2>
+	<p>
+		Choosier has been made to help you make decisions about your own images.
+	</p>
+	<p>Please just use it for that purpose.</p>
+	<p>
+		Content added to Choosier must be legal and appropriate for all audiences.
+	</p>
+	<p>Content may be removed at any time without warning or explanation.</p>
+
+	<h2>Don't expect 100% reliability during beta</h2>
+	<p>
+		Choosier is new and constantly evolving so there may be bugs or loss of
+		data.
+	</p>
+	<p>
+		If the results you get using Chooiser are important to you, make sure you
+		keep your own copy - do not rely on Choosier.
+	</p>
+	<p>I appreciate your understanding.</p>
+</template>
