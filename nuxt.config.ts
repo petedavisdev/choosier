d2b6788a544b77fccf0293f33fbae9c34af1eb6a
petedavisdev <oldmanplanet@gmail.com>
2023-01-10 22:44:50 +0000

    Add vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
 	modules: ['@nuxtjs/supabase'],
+	plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
 	runtimeConfig: {
 		public: {
 			baseUrl: process.env.SITE_URL,
