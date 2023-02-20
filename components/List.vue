e1e4719b849456ceff0c21b221846730776ed27f
petedavisdev <oldmanplanet@gmail.com>
2023-02-20 19:54:05 +0000

    smaller card images

--- components/List.vue
+++ components/List.vue
@@ -12,11 +12,11 @@
 				<img
 					class="cardImage"
 					v-for="image in choice.images"
-					:src="image"
+					:src="image.replace('h_800', 'h_320')"
 					alt=""
 					loading="lazy"
-					height="800"
-					width="800"
+					height="320"
+					width="320"
 				/>
 			</div>
 
