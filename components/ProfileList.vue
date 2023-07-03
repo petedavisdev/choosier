a13d74d8b5e9c4c3cca5271ee911758941ea1727
petedavisdev <oldmanplanet@gmail.com>
2023-07-03 17:50:43 +0100

    Add ProfileList

---
+++ components/ProfileList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+
+const profilesResponse = await supabase
+	.from('profiles')
+	.select('username')
+	.order('username', { ascending: true })
+	.not('updated_at', 'is', null);
+
+const profiles = profilesResponse.data as { username: string }[] | null;
+</script>
+
+<template>
+	<p>You are among the first {{ profiles?.length }} choosers 🙌</p>
+	<ul>
+		<li v-for="(profile, index) in profiles" :key="index">
+			<LinkTo :to="PATHS.user + profile.username">
+				@{{ profile.username }}
+			</LinkTo>
+		</li>
+	</ul>
+</template>
---
+++ pages/@*.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const profile = useProfile();
+</script>
+
+<template>
+	<template v-if="profile.userId.value">
+		<h1>Meet the choosers</h1>
+		<ProfileList />
+	</template>
+
+	<template v-else>
+		<h1>Login to meet the choosers</h1>
+		<UserLogin />
+	</template>
+</template>
