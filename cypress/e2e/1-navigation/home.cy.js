16a4a8af807b50e00c5832d60be63d2710be1d10
petedavisdev <oldmanplanet@gmail.com>
2023-12-06 21:14:20 +0000

    use baseUrl for cypress

--- cypress/e2e/1-navigation/home.cy.js
+++ cypress/e2e/1-navigation/home.cy.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe('template spec', () => {
 	it('displays a list of choices', () => {
-		cy.visit('http://localhost:3000');
+		cy.visit('/');
 		cy.get('.grid a.card[href^="/"]').should('have.length.above', 2);
 		cy.get('a[href="/"]').should('exist');
 		cy.get('a[href="/@"]').should('exist');
